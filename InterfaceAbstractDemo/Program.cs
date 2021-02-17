8a77d80a3587ffd3fc80514b66580351d8ac5ef4
Yağmur Polat <yamu_plt@hotmail.com>
2021-02-17 18:51:12 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using System;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new NeroCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Customer { DateOfBirth = new DateTime(1996, 11, 18), FirstName = "Yağmur", LastName = "Polat", NationalityId = "39325943402" });
+            Console.ReadLine();
+        }
+    }
+}
